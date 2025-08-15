3057abeb50be935f7645877123336ba113442034
silviolqm <silviolqm@gmail.com>
2025-08-15 16:15:46 -0300

    Add database startup and connection. Setup services in program.cs

--- src/Product-API-JWT/Program.cs
+++ src/Product-API-JWT/Program.cs
@@ -1,6 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Product_API_JWT.Data;
+using Product_API_JWT.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -10,5 +18,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+
+PrepDatabase.DoMigrations(app, app.Environment.IsProduction());
 
 app.Run();
